a864fb5f6f153895828ed1e4e3f0f3417a00e8eb
Ruben <ruben.decleyn@experian.com>
2021-01-19 13:31:44 +0000

    (MS)SQL implementation

---
+++ fizzbuzz.sql
@@ -0,0 +1,16 @@
+DECLARE @FizzBuzz varchar(max) =
+    (
+    STUFF(
+        (SELECT '
+' +
+            CASE WHEN Id % 15 = 0 THEN 'fizzbuzz'
+                WHEN Id % 5 = 0 THEN 'buzz'
+                WHEN Id % 3 = 0 THEN 'fizz'
+                ELSE CAST(Id AS VARCHAR(5)) END
+        FROM (SELECT TOP 100 ROW_NUMBER() OVER (ORDER BY object_id) Id
+                FROM sys.all_columns) FizzBuzz
+        ORDER BY Id
+        FOR XML PATH, TYPE
+    ).value(N'.[1]',N'nvarchar(max)'),1,1,N'')
+    );
+PRINT @FizzBuzz;
